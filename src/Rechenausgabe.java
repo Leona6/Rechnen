ebe3a8f57400e4e0170e4a259f26bbe25ab486aa
Leona Cerimi <cerimileona@gmail.com>
2023-05-02 00:04:25 +0200

    Initial commit

---
+++ src/Rechenausgabe.java
@@ -0,0 +1,27 @@
+public class Rechenausgabe {
+
+    public static void main(String args[]) {
+
+
+        int zahl1 = 1;
+        int zahl2 = 2;
+        int zahl3 = 3;
+        int zahl4 = 4;
+        int ergebnis;
+        int ergebnis2;
+        int ergebnis3;
+        int ergebnis4;
+
+        ergebnis = zahl1 + zahl2;
+        ergebnis2= zahl3-zahl2;
+        ergebnis3= zahl4*zahl3;
+        ergebnis4= zahl4/zahl2;
+
+        System.out.println("Ergebnis ist:" + ergebnis);
+        System.out.println("Ergebnis ist:" + ergebnis2);
+        System.out.println("Ergebnis ist:" + ergebnis3);
+        System.out.println("Ergebnis ist:" + ergebnis4);
+
+
+    }
+}
